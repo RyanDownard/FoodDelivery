7fc7a3841460f284dc8073a68e57e4d12a2917d6
ryanb <ryanb@LAPTOP-KQMFTSDA>
2021-09-02 13:01:42 -0600

    Add project files.

---
+++ FoodDelivery/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodDelivery.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Display(Name = "Category")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Display Order")]
+        [Required ]
+        public int DisplayOrder { get; set; }
+    }
+}
