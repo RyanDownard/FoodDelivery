0d04f960ef78ea30bc5fbda580d06dfeac81a9d0
ryanb <ryanb@LAPTOP-KQMFTSDA>
2021-09-25 12:39:03 -0600

    Start point for identity

--- Infrastructure/Data/ApplicationDbContext.cs
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ApplicationCore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Infrastructure.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
             : base(options)
